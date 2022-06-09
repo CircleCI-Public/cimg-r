a4e31fc5faeae13b805177afe0fc76592c1a2066
Jeff Chen <jeff.chen@circleci.com>
2022-06-09 14:09:32 -0700

    alpha CI version based off tidyverse and verse images

---
+++ build-images.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
+
+docker build --file 4.2/Dockerfile -t cimg/r:4.2.0 -t cimg/r:4.2 .
---
+++ push-images.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
+docker push cimg/r:4.2
+docker push cimg/r:4.2.0
