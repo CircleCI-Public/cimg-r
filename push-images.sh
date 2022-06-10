4b670050ee578d6404827f74ff5ef0bc2f757fec
Jeff Chen <jeff.chen@circleci.com>
2022-06-10 13:53:23 -0700

    cimg/r alpha [release] for R versions 4.1.3 and 4.2.0

--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
+docker push cimg/r:4.1
+docker push cimg/r:4.1.3
 docker push cimg/r:4.2
 docker push cimg/r:4.2.0
