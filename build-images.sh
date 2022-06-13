3b72bee964a08ce0c6ce18f0ec1be678e5e29ef4
Jeff Chen <jeff.chen@circleci.com>
2022-06-13 13:22:34 -0700

    [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 4.1/Dockerfile -t cimg/r:4.1.3 -t cimg/r:4.1 .
-docker build --file 4.2/Dockerfile -t cimg/r:4.2.0 -t cimg/r:4.2 .
+docker build --file 4.1/Dockerfile -t cimg/rlang:4.1.3 -t cimg/rlang:4.1 .
+docker build --file 4.2/Dockerfile -t cimg/rlang:4.2.0 -t cimg/rlang:4.2 .
